2d8a1a64c2581012e0c3c95b880771d39e2efb06
vinodgoprograms <vinodcy@hotmail.com>
2017-07-01 18:24:10 -0700

    Add files via upload

--- redisConnection/redis_client.go
+++ redisConnection/redis_client.go
@@ -1,1 +1,18 @@
+package redisConnection
 
+import (
+	"github.com/go-redis/redis"
+)
+
+var PrimesKey string = "primes"
+var PrimesMax string = "max"
+
+func GetRedisClient() *redis.Client {
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       13})
+	return client
+
+}
