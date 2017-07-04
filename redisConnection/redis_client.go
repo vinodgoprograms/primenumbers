a1a11ef715b06545a8cd6ca6e02cdfbe45ece8c6
vinodgoprograms <vinodcy@hotmail.com>
2017-07-03 21:20:44 -0700

    Add files via upload

--- redisConnection/redis_client.go
+++ redisConnection/redis_client.go
@@ -1,12 +1,27 @@
+/* This package can be used to connect to a local redis server.
+   Port = 6379
+   Password = ""
+   DB = 13
+*/
 package redisConnection
 
 import (
 	"github.com/go-redis/redis"
 )
-
+//redis key for the generated prime numbers
 var PrimesKey string = "primes"
+//redis key for the integer used to generate the prime numbers
 var PrimesMax string = "max"
 
+/* 
+   Connects to local redis server and returns a redis.Client object.
+   Refer to the package information for connection parameters.
+
+   Input:
+      - None
+   Output:
+      - instance of redis.Client
+*/
 func GetRedisClient() *redis.Client {
 
 	client := redis.NewClient(&redis.Options{
